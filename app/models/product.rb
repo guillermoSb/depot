af0cee5dd11bf549fc4256f44846a098d2519771
Guillermo Santos <guillermo.santosba@gmail.com>
2022-06-09 13:03:29 -0600

    Added validatoins for the Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true # Validates that the fields are present.
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}  # Validates that the price is greater than 0.
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
