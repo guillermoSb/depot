f270b83ef5c6a64891665334946a4c5a98b55872
Guillermo Santos <guillermo.santosba@gmail.com>
2022-06-09 13:14:18 -0600

    Unit testing for Products.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,27 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty"  do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price should be greater than 0.01" do
+    product = Product.new(title: "My book title", description: "uu", image_url: "zzz.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0.01
+    assert product.valid?
+  end
 end
