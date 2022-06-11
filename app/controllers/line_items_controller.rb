d94c397fd8af65a63507daa91ea6bbc474f0f492
Guillermo Santos <guillermo.santosba@gmail.com>
2022-06-11 12:58:31 -0600

    Only allow to empty the session cart.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product) # Add the product to the cart
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@line_item.cart) }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
