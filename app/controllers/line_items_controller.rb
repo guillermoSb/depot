c116a0173fe022a485ce8cecf1f5fd646bc42ab1
Guillermo Santos <guillermo.santosba@gmail.com>
2022-06-14 08:56:40 -0600

    Implemented highlight when a new item to the cart is added.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product) # Add the product to the cart
     respond_to do |format|
       if @line_item.save
-        format.turbo_stream
+        format.turbo_stream {@current_item = @line_item}
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
